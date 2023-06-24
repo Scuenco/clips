564411eaa8710b7cdbdf7b6cd651f623cc0efdac
Sherry Cuenco <sjcuenco@rocketmail.com>
2023-06-23 20:09:02 -0400

    Component design: modals

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit{
+  @Input() modalID = ''
+  
+  constructor(public modal: ModalService, public el: ElementRef) {
+  }
 
+  ngOnInit(): void {
+      document.body.appendChild(this.el.nativeElement)
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
+  }
 }
