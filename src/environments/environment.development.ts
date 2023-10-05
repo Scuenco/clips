c1b14849e931dd2d57b0460a7fa48a281ba9b6c9
Scuenco <Scuenco@users.noreply.github.com>
2023-10-05 13:35:54 -0400

    Updated .gitignore file

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCIiYGwNoujzRIgxT9SSJiK4Dnxf8umQ1k",
+        authDomain: "clips-b87d9.firebaseapp.com",
+        projectId: "clips-b87d9",
+        storageBucket: "clips-b87d9.appspot.com",
+        appId: "1:11488334541:web:1e39f780eff3ba218b0f84"
+      }
+};
